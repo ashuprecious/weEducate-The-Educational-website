37917f55d08da1f27af199ed6eb23d835a3b5faf
Ashutosh kumar <48045926+ashuprecious@users.noreply.github.com>
2022-04-06 15:46:30 +0530

    Add files via upload
    Hy.

---
+++ CSS/JS/resp.js
@@ -0,0 +1,10 @@
+burgar=document.querySelector('.burgar')
+navbar=document.querySelector('.navbar')
+rightNav=document.querySelector('.rightNav')
+navList=document.querySelector('.nav-list')
+
+burgar.addEventListener('click',()=>{
+navbar.class.toggle('v-class-resp');
+navbar.class.toggle('v-class-resp');
+navbar.classList.toggle('h-nav-resp');
+})
